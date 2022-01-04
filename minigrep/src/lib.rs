3c84a2e4da8f34e787f6a125ca0744753712f6ee
Edwin Zamudio <ezamudio@hawk.iit.edu>
2022-01-04 03:34:56 -0600

    finished rust book

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,7 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
-
 
 pub struct Config {
     pub query: String,
@@ -10,16 +9,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename, case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -34,7 +43,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         search_case_insensitive(&config.query, &contents)
     };
 
-
     for line in results {
         println!("{}", line);
     }
@@ -43,21 +51,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.
+        lines().
+        filter(|line| line.contains(query)).
+        collect()
 }
 
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
@@ -69,7 +69,6 @@ pub fn search_case_insensitive<'a>(
 
     results
 }
-
 
 #[cfg(test)]
 mod tests {
