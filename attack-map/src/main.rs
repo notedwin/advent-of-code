48e90d7b818669f04d49e4a02b9c2f3a35837a11
Edwin Zamudio <ezamudio@hawk.iit.edu>
2022-01-07 02:29:21 -0600

    finished timestamp and structs for data

--- attack-map/src/main.rs
+++ attack-map/src/main.rs
@@ -1,11 +1,8 @@
 use serde::{Serialize, Deserialize};
 use redis::{Client, Commands, Connection, RedisResult};
 use std::env;
-use std::collections::HashMap;
-use std::time::{SystemTime, Duration};
-use chrono::offset::Utc;
-use chrono::DateTime;
-
+use chrono::{DateTime, Utc,Local, NaiveDateTime};
+use reqwest;
 
 const API:&str = "http://ip-api.com/json/";
 const REDIS_URL:String = env::var("REDIS_URL").expect("REDIS_URL must be set");
@@ -17,30 +14,76 @@ struct Message {
     message: String,
 }
 
-fn pull_hackers() -> RedisResult<()> {
-    let now = SystemTime::now();
+#[derive(Serialize, Deserialize, Debug)]
+struct Hacker {
+    user: String,
+    ip: String,
+    lon: String,
+    lat: String,
+    time: String,
+}
 
-    let five_hours = Duration::from_secs(5*60*60);
-    let five_hours_ago = now - five_hours;
-    let hackers = con.zrange("hackers", five_hours_ago,now.to_secs() ).unwrap();
-    let hackers: Vec<String> = hackers.iter().map(|h| h.unwrap()).collect();
-    for hack in hackers {
-        let hack = serde_json::from_str(&hack).unwrap();
-        println!("{}", hack.message);
+#[derive(Serialize, Deserialize, Debug)]
+struct Location {
+    lat: String,
+    lon: String,
+}
+
+
+impl Default for Location {
+    fn default () -> Location {
+        Location{lat: "73.907".to_string(), lon: "40.7128".to_string()}
     }
+}
+
+fn pull_hackers() -> Vec<Hacker> {
+    let mut res:Vec<Hacker> = vec![];
+
+    let now:i64 = Local::now().timestamp();
+    let five_hours_ago: i64 = now - 5*60*60;
+    let hackers:Vec<String> = con.zrangebyscore("hackers", five_hours_ago, now).unwrap();
+
+    for hacker in hackers {
+        let hacker_json:String = con.get(hacker).unwrap();
+        let hacker_struct:Hacker = serde_json::from_str(&hacker_json).unwrap();
+        let time = NaiveDateTime::from_timestamp(hacker.parse::<i64>().unwrap(), 0);
+        hacker_struct.time = time.format("%H:%M:%S").to_string();
+        res.push(hacker_struct);
+    }
+    Ok(res)
+}
+
+async fn populate_redis(user: &str,ip: &str) -> Result<(), reqwest::Error>{
+    let mut data: Location = Location::default();
+
+    if con.exists(ip).unwrap() {
+        println!("{} already exists", ip);
+        data = serde_json::from_str(&con.get(ip).unwrap()).unwrap();
+
+    } else {
+        let url = format!("{}{}", API, ip);
+        data = reqwest::get(&url).await?.json().await?;
+        con.hmset(ip, &[
+            ("lat", data.lat),
+            ("lon", data.lon),
+        ]).unwrap();
+    }
+
+    let time:i64 = Local::now().timestamp();
+    con.hmset(time, &[
+        ("user", user),
+        ("ip", ip),
+        ("lat", &data.lat),
+        ("lon", &data.lon),
+    ]).unwrap();
+
+    con.zadd("hackers", time, time).unwrap();
+
     Ok(())
 }
 
-fn populate_redis(user: &str,ip: &str, r: &mut redis::Connection) -> RedisResult<()> {
-    if r.exists(ip).unwrap() {
-        println!("{} already exists", ip);
-    } else {
-        //r.set(user: &str, ip: &str).unwrap();
-        println!("{} added", ip);
-    }
+fn main(){
 
-
-    Ok(())
 }
 
 // fn handler(event,context) -> JsonValue {
@@ -65,8 +108,6 @@ fn populate_redis(user: &str,ip: &str, r: &mut redis::Connection) -> RedisResult
 //             "body": "Not Found"
 //         })
 //     }
-
-
 
 //     let ip = reqwest::get(API+).unwrap().text().unwrap();
 //     let ip = serde_json::from_str(&ip).unwrap();
